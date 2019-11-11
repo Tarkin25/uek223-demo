14263aa1121f663b2a9097690caad86d42eb1c08
Severin Weigold <severin.weigold@edu.tbz.ch>
2019-11-11 15:27:38 +0100

    milestone-2-solution

--- src/main/java/ch/noseryoung/demo/domain/product/ProductController.java
+++ src/main/java/ch/noseryoung/demo/domain/product/ProductController.java
@@ -3,6 +3,7 @@ package ch.noseryoung.demo.domain.product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,13 @@ public class ProductController {
     @GetMapping
     public ResponseEntity<String> product() {
         String message = "Hello, I am a product";
+
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> productWithId(@PathVariable("id") Long id) {
+        String message = "Hello, I am a product with ID " + id;
 
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
